e761f5908d8e174440358f1208b5519902e8c231
wpolitowski <wojciechpolitowski@protonmail.ch>
2022-12-17 12:11:17 +0100

    Improve button animation

--- src/pages/home.js
+++ src/pages/home.js
@@ -11,6 +11,7 @@ function createHomepage() {
     container.classList.add('container')
     homeInfo.classList.add('home-info')
     menuBtn.classList.add('menu-button')
+    clickListener(menuBtn)
     h2.textContent = "Hi there!"
     para.textContent = "Welcome to the place where all your dreams come true"
     para2.textContent = "...as long as they are vegan ones 😉"
@@ -19,6 +20,12 @@ function createHomepage() {
     homeInfo.append(h2, para, para2, menuBtn)
     container.appendChild(homeInfo)
     document.body.append(container)
+}
+
+function clickListener(element) {
+    element.addEventListener('click', () => {
+        element.classList.add('clicked')
+    })
 }
 
 export default createHomepage
