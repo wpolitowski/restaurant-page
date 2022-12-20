cdcf1615bf56db4dfbaef3a977ffd7bb35a8ff93
wpolitowski <wojciechpolitowski@protonmail.ch>
2022-12-20 13:36:26 +0100

    Fix scrollbar behaviour and move general css rules to main.css

--- src/functions/page-loading.js
+++ src/functions/page-loading.js
@@ -2,6 +2,7 @@ import createHeader from "../pages/header.js"
 import createHomePage from "../pages/home.js"
 import createMenu from "../pages/menu.js"
 import createContactPage from "../pages/contact.js"
+import "../styles/main.css"
 
 function loadPage(tab) {
     document.body.innerHTML = '';
